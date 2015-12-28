a9ce236a2d997dcfcb257829bca1a5d206a84244
her0m31 <eda.1380@gmail.com>
2015-12-28 19:44:27 +0900

    グラフィックなどのパフォーマンスの設定テスト

--- Assets/Scripts/Blinker.cs
+++ Assets/Scripts/Blinker.cs
@@ -4,7 +4,7 @@ using System.Collections;
 
 public class Blinker : MonoBehaviour {
 	private GameObject thisGameObject;
-	private float _Step = 0.015f;
+	private float addAlpha = 0.015f;
 
 	public new GameObject gameObject {
 		get {
@@ -13,14 +13,13 @@ public class Blinker : MonoBehaviour {
 	}
 
 	void Update() {
-		// 現在のAlpha値を取得
-		float toColor = gameObject.GetComponent<Text>().color.a;
+		float currentAlpha = gameObject.GetComponent<Text>().color.a;
 		// Alphaが0 または 1になったら増減値を反転
-		if (toColor < 0 || toColor > 1) {
-			_Step = _Step * -1;
+		if (currentAlpha < 0 || currentAlpha > 1) {
+			addAlpha *= -1;
 		}
 		// Alpha値を増減させてセット
-		gameObject.GetComponent<Text>().color = new Color(243.0f/255.0f, 156.0f/255.0f, 18.0f/255.0f, (toColor + _Step));
+		gameObject.GetComponent<Text>().color = new Color(243.0f/255.0f, 156.0f/255.0f, 18.0f/255.0f, (currentAlpha + addAlpha));
 	}
 
 	void Awake() {

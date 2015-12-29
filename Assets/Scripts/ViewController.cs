402650de58f38d59b03de5d67f72e794119bab7e
her0m31 <eda.1380@gmail.com>
2015-12-29 17:15:48 +0900

    メニューパーツの作成

---
+++ Assets/Scripts/ViewController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViewController : MonoBehaviour {
+	private RectTransform thisRectTransform;
+
+	public RectTransform rectTransform {
+		get {
+			return thisRectTransform == null ? GetComponent<RectTransform>() : thisRectTransform;
+		}
+	}
+
+	public virtual string Title {
+		get {
+			return "";
+		}
+		set {
+		}
+	}
+
+	void Awake() {
+		thisRectTransform = GetComponent<RectTransform>();
+	}
+}

3295202d06fa966ec56c6573e5c95aabf047150e
her0m31 <eda.1380@gmail.com>
2016-01-04 11:23:39 +0900

    android branch init

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class GameManager : SingletonMonoBehaviour<GameManager> {
 	private Nortification<int> score;
 	private Nortification<GameState> state;
+	private GameObject thisGameObject;
 
 	public static Nortification<int> Score {
 		get {return Instance.score;}
@@ -12,6 +13,10 @@ public class GameManager : SingletonMonoBehaviour<GameManager> {
 
 	public static Nortification<GameState> State {
 		get {return Instance.state;}
+	}
+
+	public new GameObject gameObject {
+		get {return thisGameObject == null ? thisGameObject = base.gameObject : thisGameObject;}
 	}
 
 	public enum GameState {
@@ -29,9 +34,34 @@ public class GameManager : SingletonMonoBehaviour<GameManager> {
 
 	void OnChangeGameState(GameManager.GameState state) {
 		switch(state) {
+			case GameManager.GameState.Title:
+
+			break;
+			case GameManager.GameState.Ready:
+
+			break;
+			case GameManager.GameState.Playing:
+			if(Application.platform == RuntimePlatform.Android) {
+				Everyplay.StartRecording();
+			}
+			break;
+			case GameManager.GameState.GameOver:
+			if(Application.platform == RuntimePlatform.Android) {
+				Everyplay.SetMetadata("score", Score.Value);
+				Everyplay.StopRecording();
+				Everyplay.ShowSharingModal();
+			}
+			break;
 			case GameManager.GameState.Restart:
-				SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-				break;
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+			break;
+		}
+	}
+
+	public void OnReadyForRecording(bool enabled) {
+		if(enabled) {
+			// The recording is supported
+			// gameObject.SetUpRecording();
 		}
 	}
 
@@ -42,6 +72,11 @@ public class GameManager : SingletonMonoBehaviour<GameManager> {
 
 	void Start () {
 		State.AddListener(OnChangeGameState);
+
+		// Register for the Everyplay ReadyForRecording event
+		if(Application.platform == RuntimePlatform.Android) {
+			Everyplay.ReadyForRecording += OnReadyForRecording;
+		}
 	}
 
 	void Awake() {
